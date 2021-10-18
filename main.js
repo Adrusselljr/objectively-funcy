fd6799f25358a4daa8938cf79f420c794548aefb
Aaron Russell Jr <adrusselljr@Aarons-MBP.lan>
2021-10-18 14:47:45 -0400

    modified js

--- main.js
+++ main.js
@@ -1,4 +1,72 @@
-// Your code here.
+function getFirstName(user = {}) {
+  return user.firstName
+}
+// console.log(getFirstName({firstName: 'Colon', lastName: 'Jaffe'}))
+// console.log(getFirstName({firstName: 'Petra', lastName: 'Solano'}))
+
+function getLastName(user = {}) {
+  return user.lastName
+}
+// console.log(getLastName({firstName: 'Colon', lastName: 'Jaffe'}))
+// console.log(getLastName({firstName: 'Petra', lastName: 'Solano'}))
+
+function getFullName(user = {}) {
+  return `${user.firstName} ${user.lastName}`
+}
+// console.log(getFullName({firstName: 'Colin', lastName: 'Jaffe'}))
+// console.log(getFullName({firstName: 'Petra', lastName: 'Solano'}))
+
+function setFirstName(user = {}, newFirst) {
+  user.firstName = newFirst
+  return user
+}
+// console.log(setFirstName({firstName: 'Colin', lastName: 'Jaffe'}, 'Redacted'))
+// console.log(setFirstName({firstName: 'Petra', lastName: 'Solano'}, 'Anthony'))
+
+function setAge(user = {}, newAge) {
+  user.age = newAge
+  return user
+}
+// console.log(setAge({firstName: 'Colin', lastName: 'Jaffe', age: 39}, 45))
+// console.log(setAge({firstName: 'Petra', lastName: 'Solano', age: 29}, 35))
+
+function giveBirthday(user = {}) {
+  console.log(user.age)
+  if(user.age === undefined) {
+    user.age = 1
+  }
+  else {
+    user.age += 1
+  }
+  return user
+}
+// console.log(giveBirthday({firstName: 'Colin', lastName: 'Jaffe', age: 39}))
+// console.log(giveBirthday({firstName: 'Petra', lastName: 'Solano', age: 29}))
+// console.log(giveBirthday({firstName: 'Baby', lastName: 'Jaffe'}))
+
+// const person1 = {firstName: 'Colin', lastName: 'Jaffe', married: false}
+// const person2 = {firstName: 'Petra', lastName: 'Solano', married: false}
+function marry(person1, person2) {
+  person1.married = true
+  person1.spouseName = `${person2.firstName} ${person2.lastName}`
+  person2.married = true
+  person2.spouseName = `${person1.firstName} ${person1.lastName}`
+}
+// marry(person1, person2)
+// console.log(person1)
+// console.log(person2)
+
+// const person1 = {firstName: 'Colin', lastName: 'Jaffe', married: true, spouseName: 'Petra Solano'}
+// const person2 = {firstName: 'Petra', lastName: 'Solano', married: true, spouseName: 'Colin Jaffe'}
+function divorce(person1, person2) {
+  person1.married = false
+  person2.married = false
+  delete person1.spouseName
+  delete person2.spouseName
+}
+// divorce(person1, person2)
+// console.log(person1)
+// console.log(person2)
 
 
 
